43afae13262f4c673c91e74f852a8ff146977883
kylewuzhere <kyvasquez1026@gmail.com>
2023-09-25 22:03:30 +1300

    wrap layout with session provider from nextauth route

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+// gets session from client
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export const Providers = ({ children }: Props) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
